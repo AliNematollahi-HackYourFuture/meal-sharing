b6db48408a1d02412f10c18115023643d531738e
Ali Nematollahi <ali.nematollahi2020@gmai.com>
2025-06-26 00:24:29 +0200

    implemented homework assignment

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import mealsRouter from './routers/meals.js';
 import reservationsRouter from './routers/reservations.js';
+import cors from 'cors';
+
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api',mealsRouter)
 app.use('/api',reservationsRouter)
 
-const PORT = 3000;
+const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
