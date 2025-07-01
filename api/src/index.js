77de2317fed32394fb186496b84918ab56619476
Ali Nematollahi <ali.nematollahi2020@gmai.com>
2025-07-02 01:02:51 +0200

    resolving conflict

--- api/src/index.js
+++ api/src/index.js
@@ -2,15 +2,19 @@ import express from 'express';
 import mealsRouter from './routers/meals.js';
 import reservationsRouter from './routers/reservations.js';
 import reviewsRouter from './routers/reviews.js';
+import cors from 'cors';
+
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api',mealsRouter)
 app.use('/api',reservationsRouter)
 app.use('/api',reviewsRouter)
 
-const PORT = 3000;
+const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
