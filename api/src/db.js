be67e543dfaf07f79ab32c2c43226649202edfab
Ali Nematollahi <ali.nematollahi2020@gmai.com>
2025-08-11 12:40:20 +0200

    update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -9,6 +9,9 @@ const db = knex({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
